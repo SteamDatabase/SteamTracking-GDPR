7c66b5199783049f9cf1703930a557637986c1a6
Wendy <freeman@steamdb.info>
2018-06-09 12:16:00 +0000

    ayy

--- update.sh
+++ update.sh
@@ -9,6 +9,6 @@ git add -u
 if [ $(git diff --cached | wc -l) -ne 0 ]; then
 
 git commit -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g')"
-# git push
+git push
 
 fi
